eca749cc7a5a3bf63940aaa5f89e896fb442da5e
adi-choopil <adi.peretz@aquant.io>
2026-02-26 13:31:30 +0200

    logic for endpoint A

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
 
+// new logic for endpoint A 
+ 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
